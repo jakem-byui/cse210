5eeb1ba6e2bb008bf40872530f459af0718fe2b9
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2023-09-14 16:08:35 -0700

    prep 2 grading activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Asks user for number grade
+        Console.Write("Please input your grade percentage (ex. 79): ");
+        string numGradeText = Console.ReadLine();
+
+        // Converts string to int for grade letter calculation
+        int numGrade = int.Parse(numGradeText);
+        string letterGrade = "";
+
+        if  (numGrade >= 90) 
+        {
+            letterGrade = "A";
+        }
+        else if (numGrade >= 80) 
+        {
+            letterGrade = "B";
+        }
+        else if (numGrade >= 70) 
+        {
+            letterGrade = "C";
+        }
+        else if (numGrade >= 60) 
+        {
+            letterGrade = "D";
+        }
+        else if (numGrade < 60) 
+        {
+            letterGrade = "F";
+        }
+        else
+        {
+            Console.WriteLine("Invalid entry. Try again with '00' format.");
+        }
+
+        Console.WriteLine ($"Your grade is {letterGrade}.");
+        
+        if (numGrade >= 70)
+        {
+            Console.WriteLine ("Congratulations! You passed!");
+        }
+        else if (numGrade <= 69)
+        {
+            Console.WriteLine ("Sorry! Looks like you didn't pass. Try again!");
+        }
     }
 }
