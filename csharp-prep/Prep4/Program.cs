bc8deb65a2709ed1626bdf55bf0d590ba6066192
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2023-09-20 20:20:25 -0700

    prep 4 anbd prep5 programs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Assignment
+        //Ask the user for a series of numbers, and append each one to a list. Stop when they enter 0.
+        //Once you have a list, have your program do the following:
+
+        // Core Requirements
+        //Work through these core requirements step-by-step to complete the program. Please don't skip 
+        // ahead and do the whole thing at once, because others on your team may benefit from building 
+        // the program up slowly.
+        // 1. Compute the sum, or total, of the numbers in the list.
+        // 2. Compute the average of the numbers in the list.
+        // 3. Find the maximum, or largest, number in the list.
+        
+        List<int> listNumbers = new List<int>();
+        
+        int userNumber = 100;
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            Console.Write("Enter number ('0' ends session): ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (userNumber != 0)
+            {
+                listNumbers.Add(userNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in listNumbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / listNumbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = listNumbers[0];
+
+        foreach (int number in listNumbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
