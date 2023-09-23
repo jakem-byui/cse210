e40c23b96e7363f439fb2c08cd99d5b08cbb33c2
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2023-09-22 19:05:59 -0700

    testing git commands

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         Console.Write  ("What is your first name? ");
         string firstName = Console.ReadLine();
-        Console.Write ("What is your last name? ");
+        Console.Write ("What's your last name? ");
         string lastName = Console.ReadLine();
         Console.WriteLine();
 
