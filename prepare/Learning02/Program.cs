c1656a0c6b498d9c7e1366c5695242db5928c5e3
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2023-09-28 23:13:43 -0700

    testing, practicing week 3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Field Technology Specialist";
+        job1._company = "FedEx Office";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Assistant Manager";
+        job2._company = "FedEx Office";
+        job2._startYear = 2020;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jake Matheson";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
