9212a04d4f45bac4cc1752c083a3653f8cc40b8d
Jake Matheson <131726132+jakem-byui@users.noreply.github.com>
2023-12-02 16:49:41 -0800

    Starting on Final

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -1,6 +1,6 @@
 using System;
 
-class Program
+class ProgramFinal
 {
     static void Main(string[] args)
     {
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,30 +1,9 @@
-class Program1
+using System;
+
+class ProgramSandbox
 {
     static void Main(string[] args)
     {
-        // Sample scripture
-        string reference = "John 3:16";
-        string text = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.";
 
-        // Create the scripture
-        Scripture scripture = new Scripture(reference, text);
-
-        // Display the initial scripture
-        scripture.Display();
-
-        while (true)
-        {
-            // Get user input
-            string userInput = Console.ReadLine();
-
-            if (userInput.ToLower() == "quit")
-                break;
-
-            // Hide random words and display the updated scripture
-            scripture.HideRandomWords();
-
-            if (scripture.AllWordsHidden())
-                break;
-        }
     }
 }
